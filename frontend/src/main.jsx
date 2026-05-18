7e31fedd4137db615d0a2fdccede27dd9980507c
sroopsai <roopsai.surampudi@icloud.com>
2026-05-18 15:15:42 +0530

    fix: add global and buffer polyfills for AWS Cognito compatibility

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,7 @@
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+window.global = window;
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
