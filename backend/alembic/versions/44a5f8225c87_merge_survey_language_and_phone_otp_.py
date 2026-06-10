914a8bcd973032cd68670b6f8a84bbb88525e608
srinivasodeti-axiora <srinivas.o@axioraglobalsolutions.com>
2026-06-10 15:58:25 +0530

    Fix formatting on Alembic merge migration

--- backend/alembic/versions/44a5f8225c87_merge_survey_language_and_phone_otp_.py
+++ backend/alembic/versions/44a5f8225c87_merge_survey_language_and_phone_otp_.py
@@ -9,7 +9,6 @@ Create Date: 2026-06-10 15:52:16.131690
 from typing import Sequence, Union
 
 
-
 # revision identifiers, used by Alembic.
 revision: str = "44a5f8225c87"
 down_revision: Union[str, None] = ("20d3d76d5811", "f5a6b7c8d9e0")
