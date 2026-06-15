ddfd55a3e99eaa6956cc0c426b7c16fa2512e7d7
deepthi-axiora <Deepthi.u@axioraglobalsolutions.com>
2026-06-15 16:58:03 +0530

    format alembic merge migration

--- backend/alembic/versions/a6f23d194e57_merge_heads.py
+++ backend/alembic/versions/a6f23d194e57_merge_heads.py
@@ -3,14 +3,15 @@
 Revision ID: a6f23d194e57
 Revises: 53db7b80b12b, f1a2b3c4d5e6
 Create Date: 2026-06-15 16:36:15.502379
-
 """
+
 from typing import Sequence, Union
 
-
-# revision identifiers, used by Alembic.
-revision: str = 'a6f23d194e57'
-down_revision: Union[str, None] = ('53db7b80b12b', 'f1a2b3c4d5e6')
+revision: str = "a6f23d194e57"
+down_revision: Union[str, None] = (
+    "53db7b80b12b",
+    "f1a2b3c4d5e6",
+)
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
