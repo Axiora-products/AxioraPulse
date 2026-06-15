4525c3efb40979b02e2951d079a95a6cbd2a252f
deepthi-axiora <Deepthi.u@axioraglobalsolutions.com>
2026-06-15 17:08:49 +0530

    fix alembic merge migration format

--- backend/alembic/versions/a6f23d194e57_merge_heads.py
+++ backend/alembic/versions/a6f23d194e57_merge_heads.py
@@ -5,15 +5,10 @@ Revises: 53db7b80b12b, f1a2b3c4d5e6
 Create Date: 2026-06-15 16:36:15.502379
 """
 
-from typing import Sequence, Union
-
-revision: str = "a6f23d194e57"
-down_revision: Union[str, None] = (
-    "53db7b80b12b",
-    "f1a2b3c4d5e6",
-)
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision = "a6f23d194e57"
+down_revision = ("53db7b80b12b", "f1a2b3c4d5e6")
+branch_labels = None
+depends_on = None
 
 
 def upgrade() -> None:
