09f490a31652d041816ed494e9eba4fe2afaa6e8
deepthi-axiora <Deepthi.u@axioraglobalsolutions.com>
2026-06-15 16:38:23 +0530

    merge alembic heads

---
+++ backend/alembic/versions/a6f23d194e57_merge_heads.py
@@ -0,0 +1,26 @@
+"""merge heads
+
+Revision ID: a6f23d194e57
+Revises: 53db7b80b12b, f1a2b3c4d5e6
+Create Date: 2026-06-15 16:36:15.502379
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'a6f23d194e57'
+down_revision: Union[str, None] = ('53db7b80b12b', 'f1a2b3c4d5e6')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
