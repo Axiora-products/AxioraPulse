aa6fd71670e87843f107bd2305e9a3b8f7a298c4
deepthi-axiora <Deepthi.u@axioraglobalsolutions.com>
2026-06-15 15:37:39 +0530

    remove unused postgresql import

--- backend/alembic/versions/53db7b80b12b_add_invited_by_to_user_profiles.py
+++ backend/alembic/versions/53db7b80b12b_add_invited_by_to_user_profiles.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
+
 
 # revision identifiers, used by Alembic.
 revision: str = '53db7b80b12b'
